c986341021da748e61661c44e9f1c5349e07c76c
caiogfe <130512314+caiogfe@users.noreply.github.com>
2025-06-24 12:11:25 -0300

    finalizando menu

--- desafios/d016/scripts/script.js
+++ desafios/d016/scripts/script.js
@@ -1,17 +1,20 @@
-var itens = document.querySelector('nav#menu')
+const itens = document.querySelector('nav#menu')
+const burger = document.querySelector('span#burger')
 
 function mudouTamanho() {
     if (window.innerWidth >= 769) {
-        itens.style.display = 'block'
+        itens.style.display = 'flex'
+        burger.style.display = 'none'
     } else {
         itens.style.display = 'none'
+        burger.style.display = 'flex'
     }
 }
 
 function clickMenu() {
-    if (itens.style.display == 'block') {
+    if (itens.style.display == 'flex') {
         itens.style.display = 'none'
     } else {
-        itens.style.display = 'block'
+        itens.style.display = 'flex'
     }
 }
